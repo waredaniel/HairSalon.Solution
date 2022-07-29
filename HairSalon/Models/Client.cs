fd50ee764a126010bbf1277901e9085414cd360e
danielware <waredanielb@gmail.com>
2022-07-29 10:28:13 -0700

    Create working splash page.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public string ClientDescription { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
